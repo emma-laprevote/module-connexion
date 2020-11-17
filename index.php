d7b931e0ef03ce6b0c00a0cabead7a622560ac55
Laprevote Emma <emma.laprevote@laplateforme.io>
2020-11-17 20:31:37 +0100

    rectification terminé index, inscription, connexion

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,7 +9,9 @@
     <link rel="stylesheet" type="text/css" href="connexion.css">
     <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">  
-    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">   
+ 
     
     <title>SLASH MAGAZINE</title>
 </head>
@@ -53,11 +57,47 @@
 
             <section id="freshnews">
 
+                <section id="presentadherent">
+
+                    <div id="adherent">
+                    <div>
+                    <img id="retro" src="images/giphy3.gif" alt="gif retro">
+                    </div>
+                    <section class="buttons">
+                    <?php
+                        if($_SESSION['login'] == "") {
+                            $user = $_SESSION['login'];
+                                // afficher un message
+                            echo "<h3 id='identity2'>HELLO WORLD !</h3>
+                            <a class='button2' href='connexion.php'>Connexion</a>
+                            <a class='button1' href='inscription.php'>Inscription</a>";
+
+                        } elseif ($_SESSION['login'] != "") { 
+                            $user = $_SESSION['login'];
+
+                            echo "<h3 id='identity'>HELLO $user !</h3>
+                            <a class='button2' href='profil.php'>Mon profil</a>
+                            <a class='button1' href='#'>Ecrire un article</a>";
+                        
+                        }
+                        ?>
+                    </section>
+                    </div>
+
+                    <div class="presentslash">
+                        <h1 id="titleslash">SLASH/</h1>
+                        <p id="textslash"><strong>Slash magazine</strong>, le magazine numérique dédié au journalisme libre et indépendant<br>
+                    Mettez vos talents de rédacteur au profil du savoir et de l'information en adhérent à la communauté Slasher, in order to slash the world!<br>
+                    Une plateforme 100% gratuite où règne la liberté d'expression.</p>
+                    </div>
+
+                </section>
+
                 <section id="press1">
-        
+
                     <div class="articleprothese">
                         <h6>Tech</h6>
-                        <a id="lienarticle" href="https://www.vice.com/fr/article/xgzddw/des-amputes-fabriquent-leurs-propres-protheses-pour-changer-le-monde" target="_blank"><h1>Des amputés fabriquent leurs propres prothèses pour changer le monde</h1></a>
+                        <a id="lienarticle" href="#" target="_blank"><h1>Des amputés fabriquent leurs propres prothèses pour changer le monde</h1></a>
                         <p class="p1">Après des décennies de statu quo, des activistes bardés d'imprimantes 3D font 
                         souffler un vent de changement sur l'industrie des prothèses.</p>
                         <p class="p2">Sébastien Morelli</p>
@@ -67,6 +107,7 @@
                         <img id="prothese" src="images/prothese.webp" alt="photo prohtese jambe">
                     </div>
                 </section>
+
             <section class="articlerow">
 
                 <section id="press2">
@@ -75,7 +116,7 @@
                             <img id="conchita" src="images/conchita.webp" alt="photo portrait madame pipi">
                         </div>
                         <h6>Culture</h6>
-                        <a id="lienarticle2" href="https://www.vice.com/fr/article/4advkd/conchita-la-madame-pipi-du-fuse-club-legendaire-bruxellois" target="_blank"><h3>On a tapé la discute à la madame pipi du Fuse, club légendaire bruxellois</h3></a>
+                        <a id="lienarticle2" href="#" target="_blank"><h3>On a tapé la discute à la madame pipi du Fuse, club légendaire bruxellois</h3></a>
                         <p class="p3">« Avec moi, si on n’a pas 50 centimes, on ne va pas aux toilettes, et si on est 
                             désagréable, on se prend un coup de raclette. »</p>
                         <p class="p4">Hanna Pallot</p>
@@ -89,7 +130,7 @@
                             <img id="vino" src="images/vino.webp" alt="photo domaine américain">
                         </div>
                         <h6>Food</h6>
-                        <a id="lienarticle3" href="https://www.vice.com/fr/article/3annwb/bleu-blanc-rouge-tout-ce-que-le-pinard-americain-doit-a-la-france" target="_blank"><h3>Bleu, blanc, rouge : tout ce que le pinard américain doit à la France </h3></a>
+                        <a id="lienarticle3" href="#" target="_blank"><h3>Bleu, blanc, rouge : tout ce que le pinard américain doit à la France </h3></a>
                         <p class="p3">Dans les pas des vignerons de l’Hexagone à l’origine des premières bouteilles de pif du Nouveau Monde.</p>
                         <p class="p4">Alexis Ferenczi</p>
                         <p class="p4">il y a 3 jours</p>
@@ -102,7 +143,7 @@
                             <img id="console" src="images/xbox.webp" alt="photo nouvelle xbox">
                         </div>
                         <h6>Gaming</h6>
-                        <a id="lienarticle4" href="https://www.vice.com/fr/article/wx8dn9/les-series-x-et-s-de-microsoft-eliminentles-petits-tracas-du-gamer" target="_blank"><h3>Les nouvelles Xbox ont été conçues pour éliminer les petits tracas du gamer</h3></a>
+                        <a id="lienarticle4" href="#" target="_blank"><h3>Les nouvelles Xbox ont été conçues pour éliminer les petits tracas du gamer</h3></a>
                         <p class="p3">Les Series X et S de Microsoft n’ont pas encore de nouveaux jeux, 
                         mais ça ne les empêchera pas de vous faire de l'œil avec le catalogue existant.</p>
                         <p class="p4">Rob Zacny</p>
