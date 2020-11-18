b17526cb9bd2af53519927805b0363ed095301da
Laprevote Emma <emma.laprevote@laplateforme.io>
2020-11-18 08:30:26 +0100

    Création page profil

---
+++ profil.php
@@ -0,0 +1,76 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="connexion.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+   
+ 
+    
+    <title>SLASH MAGAZINE</title>
+</head>
+
+    <body>
+
+        <header>
+            
+            <img id="logo" src="images/logoslash.png">
+
+            <section class="icones">
+
+                <a href="https://www.facebook.com/emmalaprevote"><img id="icon" src="images/facebook.PNG" alt="iconefacebook"></a>
+                  
+                  
+                <a href="https://www.linkedin.com/in/emkalaprevote"><img id="icon1" src="images/linkedin.PNG" alt="iconelinkedin"></a>
+              
+              
+                <a href="https://twitter.com"><img id="icon2" src="images/twitter.PNG" alt="iconetwitter"></a>
+              
+              
+                <a href="https://www.instagram.com/emk.a"><img id="icon3" src="images/insta.PNG" alt="iconeistagram"></a>
+              
+            </section>
+
+            <nav>
+                <a href="index.php">Accueil »</a>
+
+                <a href="inscription.php">Inscription »</a>
+
+                <a href="connexion.php">Connexion »</a>
+
+                <a class="connex" href="profil.php">Mon compte »</a>
+                
+                <a href="admin.php">Espace admin »</a>
+            </nav>
+
+        </header>
+
+    <main id="accueil">
+
+        <article>
+
+            <section id="freshnews">
+
+                <h1 id="titleprofil">Mon compte</h1>
+
+            </section>
+
+        </article>
+
+    </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+
+</html>
